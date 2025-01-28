1a88c52507dd9a05f21e649a148e2b7f2cbdddde
Ramos <159084323+CodeByRamos@users.noreply.github.com>
2025-01-28 15:06:02 -0300

    Update views.py

--- views.py
+++ views.py
@@ -1,24 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Perguntas, Escolha
-
 
 def index(request):
     ultimasquestoes = Perguntas.objects.order_by('-datadepublicacao')[:5]
     conteudo = {'ultimasquestoes': ultimasquestoes}
     return render(request, 'Enquete/index.html', conteudo)
-
-
-def resultados(request, question_id):
-    pergunta = get_object_or_404(Perguntas, pk=question_id)
-    return HttpResponse(f"Esses são os resultados da pergunta número {question_id}")
-
-
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 
 def votacao(request, question_id):
     pergunta = get_object_or_404(Perguntas, pk=question_id)
